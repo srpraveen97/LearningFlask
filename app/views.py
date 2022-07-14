128083f1a0fbb17b6652b2c531c56b2e18b8b705
Praveen Sundaresan Ramesh <srpraveen1997@gmail.com>
2022-07-14 17:45:44 -0400

    Learning Flask Part 9

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, jsonify, make_response
 from datetime import datetime
 
 @app.template_filter("clean_date")
@@ -93,3 +93,24 @@ def profile(username):
 @app.route("/multiple/<foo>/<bar>/<baz>")
 def multi(foo, bar, baz):
           return f'foo is {foo}, bar is {bar}, baz is {baz}'
+      
+      
+@app.route('/json',methods=['POST'])
+def json():
+    
+    if request.is_json:
+        req = request.get_json()
+        
+        response = {
+            "message":"JSON received",
+            "name": req.get("name")
+        }
+        
+        res = make_response(jsonify(response), 200)
+        
+        return res
+    
+    res = make_response(jsonify({"message":"No JSON received"}), 400)
+    return res
+
+
